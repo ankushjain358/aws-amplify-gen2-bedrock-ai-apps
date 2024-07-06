046efb7e973710e231e0e7788c314311b8d46945
Ankush Jain <ankushjain358@gmail.com>
2024-07-06 23:39:13 +0530

    Configured amplify on client side

---
+++ app/components/ConfigureAmplifyClientSide .tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Amplify } from "aws-amplify";
+import outputs from "../../amplify_outputs.json"
+
+Amplify.configure(outputs, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
+
+// Reference: https://docs.amplify.aws/nextjs/build-a-backend/server-side-rendering/#configure-amplify-library-for-client-side-usage
